ffc7036ca751150fcd379f90314d62af71884485
Chapman Shoop <chapman.shoop@gmail.com>
2024-07-07 11:45:52 -0500

    Extract some reused text.

--- app/Main.hs
+++ app/Main.hs
@@ -23,10 +23,16 @@ import System.FilePath (takeFileName, (</>))
 import System.FilePattern.Directory (getDirectoryFiles)
 import Text.HTML.Scalpel (Scraper, URL, attr, chroots, hasClass, manager, scrapeURLWithConfig, (@:))
 
+downloads :: FilePath
+downloads = "/home/chapman/Downloads"
+
+distroWatch :: URL
+distroWatch = "https://distrowatch.com"
+
 cleanAddedTorrents :: IO ()
 cleanAddedTorrents = do
-  alreadyAddedFiles <- getDirectoryFiles "/home/chapman/Downloads" ["*.added"]
-  mapM_ (\fileName -> removeFile ("/home/chapman/Downloads/" ++ fileName)) alreadyAddedFiles
+  alreadyAddedFiles <- getDirectoryFiles downloads ["*.added"]
+  mapM_ (\x -> removeFile (downloads </> x)) alreadyAddedFiles
 
 addUserAgent :: ManagerSettings
 addUserAgent =
@@ -44,7 +50,7 @@ addUserAgent =
 mostRecentTorrents :: IO (Maybe [URL])
 mostRecentTorrents = do
   manager <- Just <$> newManager addUserAgent
-  scrapeURLWithConfig (def {manager}) "https://distrowatch.com/dwres.php?resource=bittorrent" torrentURLs
+  scrapeURLWithConfig (def {manager}) (distroWatch </> "dwres.php?resource=bittorrent") torrentURLs
   where
     torrentURLs :: Scraper String [URL]
     torrentURLs = chroots ("td" @: [hasClass "torrent"]) torrentURL
@@ -60,13 +66,13 @@ downloadRecentTorrents = do
   let recent = take 25 $ filter (\x -> ".torrent" `isSuffixOf` x) (fromJust torrentURLs)
   mapM_
     ( \x -> do
-        let url = "https://distrowatch.com/" ++ x
+        let url = distroWatch </> x
         manager <- newManager addUserAgent
 
         request <- parseRequest url
         response <- httpLbs request manager
 
-        let filePath = "/home/chapman/Downloads" </> (takeFileName x)
+        let filePath = downloads </> (takeFileName x)
 
         writeFile filePath $ unpack (responseBody response)
     )
